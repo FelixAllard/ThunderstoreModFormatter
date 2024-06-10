cde0ae291c0a6da9c7f3291a0534025821f3f8a2
FelixAllard <xilef992@gmail.com>
2024-06-09 21:19:16 -0400

    First Commit

---
+++ ThunderstoreFormatter/SQLite/DatabaseContext/ProfileDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThunderstoreFormatter.SQLite.Model;
+
+namespace ThunderstoreFormatter.SQLite.DatabaseContext;
+
+public class ProfileDbContext : DbContext
+{
+    public DbSet<Profile> Profiles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=profiles.db");
+}
---
+++ ThunderstoreFormatter/SQLite/Model/Profile.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThunderstoreFormatter.SQLite.Model;
+
+public class Profile
+{
+    [Key]
+    public int ID { get; set; }
+    public string ProfileName { get; set; }
+    public int NumberMods { get; set; }
+    public string Path { get; set; }
+}
